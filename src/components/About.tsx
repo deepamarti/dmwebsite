e7c397deb904cc86f4dd17586e44c4ab724e33b0
deepamarti <deepa.marti@gmail.com>
2026-06-03 21:09:10 -0700

    update about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,21 +16,20 @@ export default function About() {
 
             <p>
               Outside of work, I spend a lot of my time running. I completed my first
-              marathon in June 2025 and am currently training for the Vancouver Marathon in May and NYC Marathon in November 2026. Running has become one of my favorite
-              ways to explore places, build community, and challenge myself.
+              marathon in June 2025 and am currently training the NYC Marathon in November 2026. Running has become one of my favorite
+              ways to explore new places, build community, and challenge myself.
             </p>
 
             <p>
               I also love organizing things that bring people together—from book
               club meetings to volunteer projects. Recently I’ve been fundraising for
               <span> </span><a href="https://www.gotrnyc.org/fundraiser/Deepa-M-2026TCSNYCMarathon-42780" target="_blank" rel="noopener noreferrer">Girls on the Run</a>, a nonprofit that helps young girls build confidence and community
-              through running.
+              through running. I've been coaching a local team in Puget Sound and am excited to continue supporting their mission.
             </p>
 
             <p>
-              When I’m not running or coding, you’ll probably find me writing, working
-              in my community garden plot, experimenting in the kitchen, or my
-              planning my next adventure.
+              When I’m not running or coding, you’ll probably find me crafting, gardening, experimenting in the kitchen, or planning my
+              next adventure.
             </p>
 
           </div>
